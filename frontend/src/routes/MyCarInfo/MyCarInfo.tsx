ce3ccce1294c63222b3cc0f45092903596b24a7e
HeoSeongMun <100749666+HeoSeongMun@users.noreply.github.com>
2024-11-26 20:05:45 +0900

    MyCarInfo 차량 리스트 수정중

--- frontend/src/routes/MyCarInfo/MyCarInfo.tsx
+++ frontend/src/routes/MyCarInfo/MyCarInfo.tsx
@@ -4,6 +4,7 @@ import Nav from "../../components/Nav";
 import CarAddPopup from "./CarAddPopup";
 import CarAddPopup_1 from "./CarAddPopup_1";
 import "./MyCarinfo.css";
+import "./CarList.css";
 import { useLocation } from "react-router-dom";
 
 const MyCarInfo: React.FC = () => {
@@ -17,20 +18,24 @@ const MyCarInfo: React.FC = () => {
     detail: "",
     image: "",
   });
+
   // 차량 목록 상태
   const [userCars, setUserCars] = useState<any[]>([]);
   const location = useLocation();
-  const { userId } = location.state || {};
+  const { userId, username } = location.state || {};
 
   // 백엔드에서 차량 목록을 조회
   useEffect(() => {
     if (userId) {
       axios
-        .get(`http://localhost:8080/cars/${BigInt(userId)}`)  // 차량 목록 API 호출
+        .get(`http://localhost:8080/cars/${BigInt(userId)}`) // 차량 목록 API 호출
         .then((response) => {
-          setUserCars(response.data.cars || []); // 차량 리스트 상태 업데이트
-          console.log(response.data.result.length);
-          console.log(response.data.result);
+          setUserCars(response.data.result || []); // 차량 리스트 상태 업데이트
+          console.log(
+            "response.data.result.length: ",
+            response.data.result.length
+          );
+          console.log("response.data.result: ", response.data.result);
         })
         .catch((error) => {
           console.error("차량 목록 조회 오류:", error);
@@ -38,7 +43,6 @@ const MyCarInfo: React.FC = () => {
     }
   }, [userId]);
 
-  
   // 첫 번째 팝업 열기/닫기
   const handleOpenFirstPopup = () => setIsFirstPopupOpen(true);
   const handleCloseFirstPopup = () => setIsFirstPopupOpen(false);
@@ -60,18 +64,47 @@ const MyCarInfo: React.FC = () => {
   return (
     <>
       <Nav />
-      <h2>내 차 관리</h2>
-      <div className="my-car-container">
-        <p style={{ fontWeight: "bold", fontSize: "20px" }}>
-          내 차 관리는 차매니저에서!
-        </p>
-        <p style={{ color: "#7A7A7A" }}>
-          차량을 추가하시면 다양한 정보를 한 눈에 볼 수 있습니다.
-        </p>
-        <button className="add-car-button" onClick={handleOpenFirstPopup}>
-          차량 추가
-        </button>
-      </div>
+      {userCars.length > 0 ? (
+        <div>
+          <div className="button-container">
+            <button className="add-car-button2" onClick={handleOpenFirstPopup}>
+              차량 추가
+            </button>
+          </div>
+          <h2>{username} 의 차량 정보</h2>
+          {userCars.map((car, index) => (
+            <div key={index} className="car-info-container">
+              <div className="car-info-left">
+                <h3>{car.model}</h3>
+                <p>기아 {car.model}</p>
+                <p>{car.engine} HEV 노블레스</p>
+                <p>배기량: {car.engine} cc</p>
+                <p>연료: 하이브리드({car.fuelType})</p>
+                <p>차량 등록일: {car.registrationDate}</p>
+                <p>주행 거리: {car.mileage}</p>
+              </div>
+              <div className="car-info-right">
+                <img src={car.imageUrl} alt="Car" />
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <h2>내 차 관리</h2>
+          <div className="my-car-container">
+            <p style={{ fontWeight: "bold", fontSize: "20px" }}>
+              내 차 관리는 차매니저에서!
+            </p>
+            <p style={{ color: "#7A7A7A" }}>
+              차량을 추가하시면 다양한 정보를 한 눈에 볼 수 있습니다.
+            </p>
+            <button className="add-car-button" onClick={handleOpenFirstPopup}>
+              차량 추가
+            </button>
+          </div>
+        </div>
+      )}
       {/* 첫 번째 팝업 */}
       {isFirstPopupOpen && (
         <CarAddPopup
